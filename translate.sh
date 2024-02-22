1a4bc395a66ed37fbb25466bc01a8c3372f74092
t0tal <cpt.hack1337@gmail.com>
2024-02-21 14:17:36 -1000

    Added 'delete' keyword which deletes the last entry from the wordlist. Modified help message slightly. Added another SHA256 check when backuping wordlist to be sure what SHA values should the backup wordlist have.

--- translate.sh
+++ translate.sh
@@ -9,6 +9,15 @@ then
 		exit 0
 	fi
 
+	if [[ $1 == "delete" ]]
+	then
+		$(/usr/bin/which head) -n -1 $(pwd)/words > $(pwd)/tmp
+		$(/usr/bin/which cp) $(pwd)/tmp $(pwd)/words
+		$(/usr/bin/which rm) $(pwd)/tmp
+		$(/usr/bin/which tail) -n 5 $(pwd)/words
+		exit 0
+	fi
+
 	if grep -i $1 $(pwd)/words;
 	then
 		echo "[+] Exists"
@@ -48,6 +57,8 @@ then
 	done
 
 	echo "[*] Backuping wordlist..."
+	$(/usr/bin/which sha256sum) $(pwd)/words
+	echo " "
 	$(/usr/bin/which cp) $(pwd)/words $(pwd)/words.bak
 	$(/usr/bin/which cp) $(pwd)/words.bak $(pwd)/words2.bak
 	$(/usr/bin/which cp) $(pwd)/words2.bak $(pwd)/words3.bak
@@ -55,10 +66,13 @@ then
 	$(/usr/bin/which sha256sum) $(pwd)/words*
 else
 	echo "[-] Too much or too few parameters ( min 1, max 4 needed )"
-	echo -e "[*]\t'1': check if word exists in wordlist ( 'check' as the arg to display last 5 words from wordlist )"
+	echo -e "[*]\t'1': check if word exists in wordlist"
+	echo -e "[*]\t'1': keyword 'check' - display last 5 words from wordlist"
+	echo -e "[*]\t'1': keyword 'delete' - delete last entry from wordlist"
 	echo -e "[*]\t'2': add 'word - translation' to the wordlist"
 	echo -e "[*]\t'3': add 'word - tanslation - description' to the wordlist"
-	echo -e "[*]\t'4': backup wordlist ( 'check' among them args for wordlist:backup match )"
+	echo -e "[*]\t'4': backup wordlist"
+	echo -e "[*]\t'4': 3 random params + keyword 'check' for wordlist:backup match"
 	echo -e "\n[*] You provided $# parameters"
 	exit 1
 fi
